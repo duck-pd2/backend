ec6932461297bf314aebac9be008bc3cea9d8e9b
killin0415 <swordfire1000@gmail.com>
2024-06-13 23:13:30 +0800

    resolve rebase problem

--- src/main/kotlin/xyz/potatoez/routing/EventRouting.kt
+++ src/main/kotlin/xyz/potatoez/routing/EventRouting.kt
@@ -1,43 +1,71 @@
 package xyz.potatoez.routing
 
 import io.ktor.http.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.bson.types.ObjectId
 import xyz.potatoez.application.requests.EventRequest
 import xyz.potatoez.application.requests.toDomain
 import xyz.potatoez.domain.ports.EventRepository
+import xyz.potatoez.domain.ports.UserRepository
 import xyz.potatoez.utils.ParserICS
 
-fun Route.eventRouting(repository: EventRepository) {
-    post("/register") {
-        try {
-            val formData = call.receiveParameters()
-            val url = formData["ics_url"] ?: run {
-                call.respond(HttpStatusCode.BadRequest, mapOf("message" to "Missing ICS URL"))
+fun Route.eventRouting(repository: EventRepository, userRepository: UserRepository) {
+    authenticate("auth-user") {
+        post("/events") {
+            val principal = call.principal<JWTPrincipal>() ?: run {
+                call.respond(HttpStatusCode.Unauthorized, mapOf("message" to "missing JWT token"))
+                return@post
+            }
+            val userId: ObjectId = principal.getClaim("user_id", ObjectId::class) ?: run {
+                call.respond(HttpStatusCode.BadRequest, mapOf("message" to "invalid credentials"))
+                return@post
+            }
+            try {
+                val user = userRepository.readUser(userId) ?: run {
+                    call.respond(HttpStatusCode.NotFound, mapOf("message" to "user not found"))
+                    return@post
+                }
+                
+
+            } catch (e: Exception) {
+                System.err.println(e)
+                call.respond(HttpStatusCode.InternalServerError, mapOf("message" to e.localizedMessage))
                 return@post
             }
 
-            // Parsing the ICS file to get the list of events
-            val parsedEvents = ParserICS(url)
-
-            // Storing each event into the database
-            parsedEvents.forEach { eventMap ->
-                val eventRequest = EventRequest(eventMap)
-                val event = eventRequest.toDomain()
-                val id = repository.createEvent(event)
-
-                if (id == null) {
-                    call.respond(HttpStatusCode.BadRequest, mapOf("message" to "Database error for event: ${event.title}"))
+            try {
+                val formData = call.receiveParameters()
+                val url = formData["ics_url"] ?: run {
+                    call.respond(HttpStatusCode.BadRequest, mapOf("message" to "Missing ICS URL"))
                     return@post
                 }
+
+                // Parsing the ICS file to get the list of events
+                val parsedEvents = ParserICS(url)
+
+                // Storing each event into the database
+                parsedEvents.forEach { eventMap ->
+                    val eventRequest = EventRequest(eventMap)
+                    val event = eventRequest.toDomain()
+                    val id = repository.createEvent(event)
+
+                    if (id == null) {
+                        call.respond(HttpStatusCode.InternalServerError, mapOf("message" to "Database error for event: ${event.title}"))
+                        return@post
+                    }
+                }
+
+                call.respond(HttpStatusCode.Created, mapOf("data" to parsedEvents))
+
+            } catch (e: Exception) {
+                System.err.println(e)
+                call.respond(HttpStatusCode.InternalServerError, mapOf("message" to e.localizedMessage))
             }
-
-            call.respond(HttpStatusCode.Created, mapOf("message" to "Events registered successfully"))
-
-        } catch (e: Exception) {
-            System.err.println(e)
-            call.respond(HttpStatusCode.InternalServerError, mapOf("message" to e.localizedMessage))
         }
     }
+
 }
--- src/test/kotlin/xyz/potatoez/ApplicationTest.kt
+++ src/test/kotlin/xyz/potatoez/ApplicationTest.kt
@@ -1,16 +1,14 @@
 package xyz.potatoez
 
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import io.ktor.server.testing.*
-import kotlin.test.*
-import xyz.potatoez.plugins.*
+import xyz.potatoez.application.requests.EventRequest
+import xyz.potatoez.application.requests.toDomain
+import xyz.potatoez.domain.entity.Event
 import xyz.potatoez.utils.ParserICS
 
 fun main (){
-    //val MapList : MutableList<MutableMap<String,String>> = ArrayList()
-    val url :String =  "https://moodle.ncku.edu.tw/calendar/export_execute.php?userid=204431&authtoken=eaa2da79919eefaf40536173ff47f443d2012ae2&preset_what=all&preset_time=custom"
-    val MapList = ParserICS(url)
-    println(MapList)
+    val url: String =  "https://moodle.ncku.edu.tw/calendar/export_execute.php?userid=204431&authtoken=eaa2da79919eefaf40536173ff47f443d2012ae2&preset_what=all&preset_time=custom"
+    val mapList = ParserICS(url)
+    val m = mapList[0]
+    val e: Event = EventRequest(m).toDomain()
+    println(e)
 }
