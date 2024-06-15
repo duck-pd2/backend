f8f4dfa2a62c574cba5b5ec636a51d531a747bdb
potato <swordfire1000@gmail.com>
2024-06-15 16:29:52 +0800

    adding CORS

--- src/main/kotlin/xyz/potatoez/plugins/HTTP.kt
+++ src/main/kotlin/xyz/potatoez/plugins/HTTP.kt
@@ -5,7 +5,6 @@ import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cachingheaders.*
 import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.response.*
 
 fun Application.configureHTTP() {
     install(CORS) {
@@ -14,6 +13,8 @@ fun Application.configureHTTP() {
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
         allowHeader(HttpHeaders.Authorization)
+        allowHeader(HttpHeaders.AccessControlAllowOrigin)
+        allowHeader(HttpHeaders.ContentType)
         allowHeader("MyCustomHeader")
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
